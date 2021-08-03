becea715455966937cff82cd6a39d8764994f853
Alex <69160299+alexnoot@users.noreply.github.com>
2021-08-03 18:03:52 +0200

    Add missing field for Aidon 6534

--- aidon_forward.py
+++ aidon_forward.py
@@ -14,33 +14,35 @@ args = parser.parse_args()
 
 # Class used to forward data to Home Assistant and InfluxDB
 hi = hass_influx(
-	inf_host=args.influx_host,
-	inf_db=args.influx_db,
-	hass_host=args.hass_host,
-	hass_token=args.hass_token)
+    inf_host=args.influx_host,
+    inf_db=args.influx_db,
+    hass_host=args.hass_host,
+    hass_token=args.hass_token)
 
 ser = serial.Serial(args.serial_port, 2400, timeout=0.05, parity=serial.PARITY_NONE)
 
+
 def aidon_callback(fields):
-	ts = time.time()
+    ts = time.time()
 
-	if 'p_act_in' in fields:
-		hi.post("aidon", "power", "%.03f" % (fields['p_act_in']/1000.0), hass_name="Effekt", hass_unit="kW", ts=ts)
+    if 'p_act_in' in fields:
+        hi.post("aidon", "power", "%.03f" % (fields['p_act_in'] / 1000.0), hass_name="Power", hass_unit="kW", ts=ts)
 
-	if ('il1' in fields) and args.influx_host:
-		hi.post_influx("voltage", "aidon_p1", "%.01f" % fields['ul1'], ts=ts)
-		hi.post_influx("voltage", "aidon_p2", "%.01f" % fields['ul2'], ts=ts)
-		hi.post_influx("voltage", "aidon_p3", "%.01f" % fields['ul3'], ts=ts)
-		hi.post_influx("current", "aidon_p1", "%.01f" % fields['il1'], ts=ts)
-		hi.post_influx("current", "aidon_p2", "%.01f" % fields['il2'], ts=ts)
+    if ('il1' in fields) and args.influx_host:
+        hi.post_influx("voltage", "aidon_p1", "%.01f" % fields['ul1'], ts=ts)
+        hi.post_influx("voltage", "aidon_p2", "%.01f" % fields['ul2'], ts=ts)
+        hi.post_influx("voltage", "aidon_p3", "%.01f" % fields['ul3'], ts=ts)
+        hi.post_influx("current", "aidon_p1", "%.01f" % fields['il1'], ts=ts)
+        hi.post_influx("current", "aidon_p2", "%.01f" % fields['il2'], ts=ts)
+        hi.post_influx("current", "aidon_p3", "%.01f" % fields['il3'], ts=ts)
 
-	if 'energy_act_in' in fields:
-		hi.post("aidon", "energy", "%.02f" % fields['energy_act_in'], hass_name="Energi", hass_unit="kWh", ts=ts)
+    if 'energy_act_in' in fields:
+        hi.post("aidon", "energy", "%.02f" % fields['energy_act_in'], hass_name="Energy", hass_unit="kWh", ts=ts)
+
 
 a = aidon(aidon_callback)
-				
-while(1):
-	while ser.inWaiting():
-		a.decode(ser.read(1))
-	time.sleep(0.01)
 
+while (1):
+    while ser.inWaiting():
+        a.decode(ser.read(1))
+    time.sleep(0.01)
